7a6cca64c9d11c4336b07263efe139ab006127d1
Vladyslav Rohalov <domus.lg@gmail.com>
2023-04-02 17:15:55 +0200

    Added camera to app

--- Screens/posts/createPostsScreen.jsx
+++ Screens/posts/createPostsScreen.jsx
@@ -8,9 +8,11 @@ import {
   Keyboard,
   TextInput,
 } from 'react-native';
-import Svg, { Path, Rect, G, Defs, ClipPath, Circle } from 'react-native-svg';
-import { useState, useRef } from 'react';
+import React from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { useState, useRef, useEffect } from 'react';
 import { Camera } from 'expo-camera';
+import { CameraIcon, DeleteIcon, LocationIcon } from '../../utils/icons';
 
 const initialState = {
   photo: '',
@@ -21,11 +23,14 @@ const initialState = {
 export default function CreatePostScreen({ navigation }) {
   const [publicationData, setPublicationData] = useState('');
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
-  // const [camera, setCamera] = useState(null);
-  // const [photo, setPhoto] = useState(null);
-  // const [location, setLocation] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [location, setLocation] = useState(null);
   const inputTitleRef = useRef();
   const inputLocationRef = useRef();
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  useFocusEffect(React.useCallback(() => {}, []));
 
   const handlePublication = () => {
     setIsShowKeyboard(false);
@@ -33,17 +38,20 @@ export default function CreatePostScreen({ navigation }) {
     setPublicationData(initialState);
     inputTitleRef.current.clear();
     inputLocationRef.current.clear();
+
+    console.log(permission);
   };
 
-  // const takePhoto = async () => {
-  //   try {
-  //     const photo = await camera.takePictureAsync();
-  //     setPhoto(photo.uri);
-  //     getAddress();
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const takePhoto = async () => {
+    try {
+      const photo = await camera.takePictureAsync();
+      setPhoto(photo.uri);
+      // getAddress();
+      console.log(photo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -51,41 +59,10 @@ export default function CreatePostScreen({ navigation }) {
         <KeyboardAvoidingView style={{ flex: 1 }}>
           <View style={styles.main}>
             <View style={styles.cameraWrapper}>
-              <Camera style={styles.cameraContainer}>
-                <TouchableOpacity onPress={() => {}}>
-                  <Text style={{ color: 'white' }}>SNAP</Text>
+              <Camera style={styles.cameraContainer} ref={setCamera}>
+                <TouchableOpacity onPress={takePhoto}>
+                  <CameraIcon />
                 </TouchableOpacity>
-                {/* <TouchableOpacity style={styles.uploadPhoto}>
-                <Svg
-                  width="60"
-                  height="60"
-                  viewBox="0 0 60 60"
-                  fill="none"
-                  style={styles.svgPhoto}
-                >
-                  <Circle cx="30" cy="30" r="30" fill="white" />
-                  <G clipPath="url(#clip0_32_29)">
-                    <Path
-                      d="M29.9998 33.2C31.7671 33.2 33.1998 31.7673 33.1998 30C33.1998 28.2327 31.7671 26.8 29.9998 26.8C28.2325 26.8 26.7998 28.2327 26.7998 30C26.7998 31.7673 28.2325 33.2 29.9998 33.2Z"
-                      fill="#BDBDBD"
-                    />
-                    <Path
-                      d="M27 20L25.17 22H22C20.9 22 20 22.9 20 24V36C20 37.1 20.9 38 22 38H38C39.1 38 40 37.1 40 36V24C40 22.9 39.1 22 38 22H34.83L33 20H27ZM30 35C27.24 35 25 32.76 25 30C25 27.24 27.24 25 30 25C32.76 25 35 27.24 35 30C35 32.76 32.76 35 30 35Z"
-                      fill="#BDBDBD"
-                    />
-                  </G>
-                  <Defs>
-                    <ClipPath id="clip0_32_29">
-                      <Rect
-                        width="24"
-                        height="24"
-                        fill="white"
-                        transform="translate(18 18)"
-                      />
-                    </ClipPath>
-                  </Defs>
-                </Svg>
-              </TouchableOpacity> */}
               </Camera>
             </View>
 
@@ -106,30 +83,9 @@ export default function CreatePostScreen({ navigation }) {
             </View>
 
             <View>
-              <Svg
-                style={styles.svgLocation}
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
-                <Path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M20 10.3636C20 16.0909 12 21 12 21C12 21 4 16.0909 4 10.3636C4 6.29681 7.58172 3 12 3C16.4183 3 20 6.29681 20 10.3636V10.3636Z"
-                  stroke="#BDBDBD"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <Path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M12 14C13.6569 14 15 12.6569 15 11C15 9.34315 13.6569 8 12 8C10.3431 8 9 9.34315 9 11C9 12.6569 10.3431 14 12 14Z"
-                  stroke="#BDBDBD"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </Svg>
+              <View style={styles.locationIcon}>
+                <LocationIcon />
+              </View>
               <TextInput
                 style={[styles.input, { paddingLeft: 24 }]}
                 inputMode="text"
@@ -158,38 +114,7 @@ export default function CreatePostScreen({ navigation }) {
               }}
             >
               <TouchableOpacity>
-                <Svg width="70" height="40" viewBox="0 0 70 40" fill="none">
-                  <G clipPath="url(#clip0_38_39)">
-                    <Rect width="70" height="40" rx="20" fill="#F6F6F6" />
-                    <Path
-                      d="M26 14H28H44"
-                      stroke="#BDBDBD"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <Path
-                      d="M42.5 14C42.5 13.7239 42.2761 13.5 42 13.5C41.7239 13.5 41.5 13.7239 41.5 14H42.5ZM28.5 14C28.5 13.7239 28.2761 13.5 28 13.5C27.7239 13.5 27.5 13.7239 27.5 14H28.5ZM30.5 14C30.5 14.2761 30.7239 14.5 31 14.5C31.2761 14.5 31.5 14.2761 31.5 14H30.5ZM38.5 14C38.5 14.2761 38.7239 14.5 39 14.5C39.2761 14.5 39.5 14.2761 39.5 14H38.5ZM41.5 14V28H42.5V14H41.5ZM41.5 28C41.5 28.8284 40.8284 29.5 40 29.5V30.5C41.3807 30.5 42.5 29.3807 42.5 28H41.5ZM40 29.5H30V30.5H40V29.5ZM30 29.5C29.1716 29.5 28.5 28.8284 28.5 28H27.5C27.5 29.3807 28.6193 30.5 30 30.5V29.5ZM28.5 28V14H27.5V28H28.5ZM31.5 14V12H30.5V14H31.5ZM31.5 12C31.5 11.1716 32.1716 10.5 33 10.5V9.5C31.6193 9.5 30.5 10.6193 30.5 12H31.5ZM33 10.5H37V9.5H33V10.5ZM37 10.5C37.8284 10.5 38.5 11.1716 38.5 12H39.5C39.5 10.6193 38.3807 9.5 37 9.5V10.5ZM38.5 12V14H39.5V12H38.5Z"
-                      fill="#BDBDBD"
-                    />
-                    <Path
-                      d="M33 19V25"
-                      stroke="#BDBDBD"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <Path
-                      d="M37 19V25"
-                      stroke="#BDBDBD"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </G>
-                  <Defs>
-                    <ClipPath id="clip0_38_39">
-                      <Rect width="70" height="40" fill="white" />
-                    </ClipPath>
-                  </Defs>
-                </Svg>
+                <DeleteIcon />
               </TouchableOpacity>
             </View>
           </View>
@@ -210,27 +135,19 @@ const styles = StyleSheet.create({
     marginRight: 16,
     marginTop: 32,
   },
-  svgPhoto: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: [{ translateX: -30 }, { translateY: -30 }],
-  },
   cameraWrapper: {
     width: '100%',
     height: 240,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 8,
-    backgroundColor: 'red',
+    backgroundColor: '#E8E8E8',
   },
   cameraContainer: {
-    width: '90%',
-    height: '90%',
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 8,
-    zIndex: 3,
   },
   uploadPhoto: {
     backgroundColor: '#E8E8E8',
@@ -256,7 +173,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginTop: 32,
   },
-  svgLocation: { position: 'absolute', top: 36, left: 0 },
+  locationIcon: { position: 'absolute', top: 36, left: 0 },
   button: {
     height: 50,
     marginTop: 32,
--- utils/icons.jsx
+++ utils/icons.jsx
@@ -134,10 +134,47 @@ export function SignOut() {
   );
 }
 
+export function DeleteIcon() {
+  return (
+    <Svg width="70" height="40" viewBox="0 0 70 40" fill="none">
+      <G clipPath="url(#clip0_38_39)">
+        <Rect width="70" height="40" rx="20" fill="#F6F6F6" />
+        <Path
+          d="M26 14H28H44"
+          stroke="#BDBDBD"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <Path
+          d="M42.5 14C42.5 13.7239 42.2761 13.5 42 13.5C41.7239 13.5 41.5 13.7239 41.5 14H42.5ZM28.5 14C28.5 13.7239 28.2761 13.5 28 13.5C27.7239 13.5 27.5 13.7239 27.5 14H28.5ZM30.5 14C30.5 14.2761 30.7239 14.5 31 14.5C31.2761 14.5 31.5 14.2761 31.5 14H30.5ZM38.5 14C38.5 14.2761 38.7239 14.5 39 14.5C39.2761 14.5 39.5 14.2761 39.5 14H38.5ZM41.5 14V28H42.5V14H41.5ZM41.5 28C41.5 28.8284 40.8284 29.5 40 29.5V30.5C41.3807 30.5 42.5 29.3807 42.5 28H41.5ZM40 29.5H30V30.5H40V29.5ZM30 29.5C29.1716 29.5 28.5 28.8284 28.5 28H27.5C27.5 29.3807 28.6193 30.5 30 30.5V29.5ZM28.5 28V14H27.5V28H28.5ZM31.5 14V12H30.5V14H31.5ZM31.5 12C31.5 11.1716 32.1716 10.5 33 10.5V9.5C31.6193 9.5 30.5 10.6193 30.5 12H31.5ZM33 10.5H37V9.5H33V10.5ZM37 10.5C37.8284 10.5 38.5 11.1716 38.5 12H39.5C39.5 10.6193 38.3807 9.5 37 9.5V10.5ZM38.5 12V14H39.5V12H38.5Z"
+          fill="#BDBDBD"
+        />
+        <Path
+          d="M33 19V25"
+          stroke="#BDBDBD"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <Path
+          d="M37 19V25"
+          stroke="#BDBDBD"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </G>
+      <Defs>
+        <ClipPath id="clip0_38_39">
+          <Rect width="70" height="40" fill="white" />
+        </ClipPath>
+      </Defs>
+    </Svg>
+  );
+}
+
 export function CameraIcon() {
   return (
     <Svg width="60" height="60" viewBox="0 0 60 60" fill="none">
-      <Circle cx="30" cy="30" r="30" fill="white" />
+      <Circle cx="30" cy="30" r="30" fill="#FFFFFF4D" />
       <G clipPath="url(#clip0_32_29)">
         <Path
           d="M29.9998 33.2C31.7671 33.2 33.1998 31.7673 33.1998 30C33.1998 28.2327 31.7671 26.8 29.9998 26.8C28.2325 26.8 26.7998 28.2327 26.7998 30C26.7998 31.7673 28.2325 33.2 29.9998 33.2Z"
@@ -153,11 +190,34 @@ export function CameraIcon() {
           <Rect
             width="24"
             height="24"
-            fill="white"
+            fill="red"
             transform="translate(18 18)"
           />
         </ClipPath>
       </Defs>
+    </Svg>
+  );
+}
+
+export function LocationIcon() {
+  return (
+    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <Path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M20 10.3636C20 16.0909 12 21 12 21C12 21 4 16.0909 4 10.3636C4 6.29681 7.58172 3 12 3C16.4183 3 20 6.29681 20 10.3636V10.3636Z"
+        stroke="#BDBDBD"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <Path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 14C13.6569 14 15 12.6569 15 11C15 9.34315 13.6569 8 12 8C10.3431 8 9 9.34315 9 11C9 12.6569 10.3431 14 12 14Z"
+        stroke="#BDBDBD"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </Svg>
   );
 }
